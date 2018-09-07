6a29883feb4cfb64090dbc58c517a01899dbfe13
kalyc <k_chendke@hotmail.com>
2018-09-07 14:22:03 -0700

    Add sparse concat operator support

--- tests/keras/backend/mxnet_sparse_test.py
+++ tests/keras/backend/mxnet_sparse_test.py
@@ -104,6 +104,35 @@ class TestMXNetSparse(object):
         assert k_s.shape == k_d.shape
         assert_allclose(k_s, k_d, atol=1e-05)
 
+    def test_sparse_concat(self):
+        x_d = np.array([0, 7, 2, 3], dtype=np.float32)
+        x_r = np.array([0, 2, 2, 3], dtype=np.int64)
+        x_c = np.array([4, 3, 2, 3], dtype=np.int64)
+
+        x_sparse_1 = sparse.csr_matrix((x_d, (x_r, x_c)), shape=(4, 5))
+
+        x_d = np.array([0, 7, 2, 3], dtype=np.float32)
+        x_r = np.array([0, 2, 2, 3], dtype=np.int64)
+        x_c = np.array([4, 3, 2, 3], dtype=np.int64)
+
+        x_sparse_2 = sparse.csr_matrix((x_d, (x_r, x_c)), shape=(4, 5))
+
+        assert K.is_sparse(K.variable(x_sparse_1))
+        assert K.is_sparse(K.variable(x_sparse_2))
+        x_dense_1 = x_sparse_1.toarray()
+        x_dense_2 = x_sparse_2.toarray()
+
+        k_s = K.concatenate(tensors=[K.variable(x_sparse_1), K.variable(x_sparse_2)])
+        assert K.is_sparse(k_s)
+
+        k_s_d = K.eval(k_s)
+
+        # mx.sym.sparse.concat only supported for axis=0
+        k_d = K.eval(K.concatenate(tensors=[K.variable(x_dense_1), K.variable(x_dense_2)], axis=0))
+
+        assert k_s_d.shape == k_d.shape
+        assert_allclose(k_s_d, k_d, atol=1e-05)
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
