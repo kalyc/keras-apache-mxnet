c723802498cad4f743b90c279cb0c49f96e9ce8b
kalyc <k_chendke@hotmail.com>
2018-09-11 14:51:15 -0700

    Add condition to check if KerasSymbol has bind values before checking for is_sparse

--- benchmark/sparse/linear_regression/keras_sparse_model.py
+++ benchmark/sparse/linear_regression/keras_sparse_model.py
@@ -8,30 +8,25 @@ from keras import Model
 from keras.layers import Dense, Input
 from keras.optimizers import SGD
 from keras import backend as K
-import mxnet as mx
 
 
 def run_benchmark(train_data, train_label, eval_data, eval_label, batch_size, epochs):
-    inputs = Input(shape=(10,), sparse=True)
-    #print(K.is_sparse(inputs))
-    #inputs = K.variable(train_data)
+    inputs = Input(shape=(10,), dtype='float32', sparse=True)
     predictions = Dense(10, activation='linear')(inputs)
     model = Model(outputs=predictions, inputs=inputs)
+    model.summary()
 
-    if K.backend() == 'mxnet':
-        sgd = SGD(lr=0.01, momentum=0.01, lazy_update=False)
+    sgd = SGD(lr=0.01, momentum=0.01)
 
-    elif K.backend() == 'tensorflow':
-        sgd = SGD(lr=0.01, momentum=0.01)
-   
-    model.compile(loss='mse', optimizer=sgd)
+    model.compile(loss='mse', optimizer=sgd, metrics=['accuracy'])
 
     start = time.time()
     model.fit(train_data, train_label,
               epochs=epochs,
               batch_size=batch_size)
 
-    print("Keras-TF Sparse Benchmark Results")
+    print("Keras Sparse Benchmark Results")
+    print("Backend: ", K.backend())
     print("Batch Size")
     print(batch_size)
     print('Total Time')
@@ -41,3 +36,4 @@ def run_benchmark(train_data, train_label, eval_data, eval_label, batch_size, ep
 
     print("Achieved {0:.6f} validation MSE".format(mse[0]))
     print(model.evaluate(eval_data, eval_label, verbose=1, batch_size=batch_size))
+
--- benchmark/sparse/linear_regression/run_sparse_benchmark.py
+++ benchmark/sparse/linear_regression/run_sparse_benchmark.py
@@ -4,9 +4,9 @@ Prepare data for running benchmark on sparse linear regression model
 
 import argparse
 
-import keras_tf_model
-import mxnet as mx
+import keras_sparse_model
 import mxnet_sparse_model
+import mxnet as mx
 from scipy import sparse
 
 
@@ -16,13 +16,13 @@ def invoke_benchmark(batch_size, epochs):
     eval_data = mx.test_utils.rand_ndarray(shape=(1000, 10), stype='csr')
     eval_label = 3 * eval_data
 
-    print("Running Keras-TF sparse benchmark script")
-    keras_tf_model.run_benchmark(train_data=sparse.csr_matrix(train_data.asnumpy()),
-                                 train_label=sparse.csr_matrix(train_label.asnumpy()),
-                                 eval_data=sparse.csr_matrix(eval_data.asnumpy()),
-                                 eval_label=sparse.csr_matrix(eval_label.asnumpy()),
-                                 batch_size=int(batch_size),
-                                 epochs=int(epochs))
+    print("Running Keras sparse benchmark script")
+    keras_sparse_model.run_benchmark(train_data=sparse.csr_matrix(train_data.asnumpy()),
+                                     train_label=sparse.csr_matrix(train_label.asnumpy()),
+                                     eval_data=sparse.csr_matrix(eval_data.asnumpy()),
+                                     eval_label=sparse.csr_matrix(eval_label.asnumpy()),
+                                     batch_size=int(batch_size),
+                                     epochs=int(epochs))
 
     print("Running MXNet sparse benchmark script")
     mxnet_sparse_model.run_benchmark(train_data=train_data, train_label=train_label, eval_data=eval_data,
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch', default=1,
                         help='Batch of data to be processed for training')
-    parser.add_argument('--epochs', default=1000,
+    parser.add_argument('--epochs', default=10,
                         help='Number of epochs to train the model on. Set epochs>=1000 for the best results')
     args = parser.parse_args()
 
